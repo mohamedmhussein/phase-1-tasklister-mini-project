305a31634b2d05adaaa1321b2bd0365828ce01a0
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-04-19 18:06:35 -0400

    pending extra deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.querySelector("form")
+form.addEventListener('submit',(e) => {
+  e.preventDefault()
+  handleTask(e.target.new_task_description.value)
+  form.reset()
+})
+
+function handleTask(task){
+//Listing tasks
+let element = document.createElement("li")
+element.textContent = `${task}  `
+document.getElementById("tasks").append(element)
+
+// adding x buttons
+let btn = document.createElement("button")
+btn.textContent = "X"
+element.appendChild(btn)
+
+//Deleting tasks
+btn.addEventListener('click', DeleteTasks)
+
+}
+
+function DeleteTasks(e){
+e.target.parentNode.remove()
+
+}
+
 });
